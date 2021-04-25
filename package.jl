2c53b1a2ec238907436a586d6fca1feffd6a39f6
sbutron <dino.butron@gmail.com>
2021-04-25 15:54:05 -0400

    remove $PORT from Procfile

--- package.jl
+++ package.jl
@@ -1,4 +1,3 @@
 using Pkg; Pkg.activate(".")
 Pkg.add("Pluto")       #Do this as many times as you need to, changing the string (containing the package name) every time.
 Pkg.add("PlutoUI")
-import Pluto

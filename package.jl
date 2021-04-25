de26fbcfdd48b77d66d49d65ab7380472c974e72
sbutron <dino.butron@gmail.com>
2021-04-25 17:35:23 -0400

    Forgot ) in Pkg add Interact

--- package.jl
+++ package.jl
@@ -1,7 +1,7 @@
 using Pkg; Pkg.activate(".")
 Pkg.add("Pluto")       #Do this as many times as you need to, changing the string (containing the package name) every time.
 Pkg.add("PlutoUI")
-Pkg.add("Interact"
+Pkg.add("Interact")
 Pkg.add("Mux")
 import Pluto
 using Interact, Mux

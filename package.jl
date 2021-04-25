e799f860b8d80a5b23c7b12cccb36be4ccea34a1
sbutron <dino.butron@gmail.com>
2021-04-25 18:03:50 -0400

    Adding Genie

--- package.jl
+++ package.jl
@@ -1,11 +1,24 @@
 using Pkg; Pkg.activate(".")
 Pkg.add("Pluto")       #Do this as many times as you need to, changing the string (containing the package name) every time.
 Pkg.add("PlutoUI")
-Pkg.add("Interact")
-Pkg.add("Mux")
-import Pluto
-using Interact, Mux
-b = button("Click me")
-ui = hbox(b, observe(b))
-port = parse(Int64, ARGS[1])
-wait(WebIO.webio_serve(page("/", req -> ui), port))
+Pkg.add("Genie")
+Pkg.add("Genie.Router")
+using Genie
+using Genie.Router
+
+function launchServer(port)
+
+    Genie.config.run_as_server = true
+    Genie.config.server_host = "0.0.0.0"
+    Genie.config.server_port = port
+
+    println("port set to $(port)")
+
+    route("/") do
+        "Hi there!"
+    end
+
+    Genie.AppServer.startup()
+end
+
+launchServer(parse(Int, ARGS[1]))

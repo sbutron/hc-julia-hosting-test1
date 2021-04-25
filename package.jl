e575523b7518067b01fb0e637d20c5c728424c67
sbutron <dino.butron@gmail.com>
2021-04-25 17:30:19 -0400

    Interact Mux test

--- package.jl
+++ package.jl
@@ -1,4 +1,11 @@
 using Pkg; Pkg.activate(".")
 Pkg.add("Pluto")       #Do this as many times as you need to, changing the string (containing the package name) every time.
 Pkg.add("PlutoUI")
-println("Hello")
+Pkg.add("Interact"
+Pkg.add("Mix")
+import Pluto
+using Interact, Mux
+b = button("Click me")
+ui = hbox(b, observe(b))
+port = parse(Int64, ARGS[1])
+wait(WebIO.webio_serve(page("/", req -> ui), port))

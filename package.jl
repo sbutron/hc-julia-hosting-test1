a6ad91bde24d5df76f95156a52daed7206a5bdbf
sbutron <dino.butron@gmail.com>
2021-04-25 17:14:22 -0400

    Added test message

--- package.jl
+++ package.jl
@@ -1,3 +1,4 @@
 using Pkg; Pkg.activate(".")
 Pkg.add("Pluto")       #Do this as many times as you need to, changing the string (containing the package name) every time.
 Pkg.add("PlutoUI")
+println("Hello")

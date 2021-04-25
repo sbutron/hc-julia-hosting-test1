d47c37c4ae71b5ecd4e9f879d514ea984af6b7cc
sbutron <dino.butron@gmail.com>
2021-04-25 12:06:12 -0400

    remove automatic pluto.run

--- package.jl
+++ package.jl
@@ -2,4 +2,3 @@ using Pkg; Pkg.activate(".")
 Pkg.add("Pluto")       #Do this as many times as you need to, changing the string (containing the package name) every time.
 Pkg.add("PlutoUI")
 import Pluto
-Pluto.run()

eb81a932d227412353de0e7ea99cdbfe54bfe051
sbutron <dino.butron@gmail.com>
2021-04-26 06:30:55 -0400

    @async Pluto.run()

--- package.jl
+++ package.jl
@@ -1,14 +1,6 @@
 using Pkg; Pkg.activate(".")
-Pkg.add("Mux")       #Do this as many times as you need to, changing the string (containing the package name) every time.
-using Mux
+Pkg.add("Pluto")       #Do this as many times as you need to, changing the string (containing the package name) every time.
+import Pluto
 
-@app test = (
-  Mux.defaults,
-  page(respond("<h1>Hello JuliaCon!</h1>")),
-  page("/about",
-       probabilty(0.1, respond("<h1>Boo!</h1>")),
-       respond("<h1>About Me</h1>")),
-  page("/user/:user", req -> "<h1>Hello, $(req[:params][:user])!</h1>"),
-  Mux.notfound())
 
-fetch(serve(test,parse(Int,ARGS[1])))
+@async Pluto.run(host = "0.0.0.0", port = hostparse(Int,ARGS[1]))

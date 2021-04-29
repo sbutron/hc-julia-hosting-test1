bbdecdf892706252e36cfebabfb0286453ad4493
sbutron <dino.butron@gmail.com>
2021-04-29 15:31:12 -0400

    Run without code

--- app.jl
+++ app.jl
@@ -1,3 +1,3 @@
 @info "Launching Pluto"
 import Pluto
-Pluto.run(host = "0.0.0.0", port = parse(Int,ARGS[1]))
+Pluto.run(host = "0.0.0.0", port = parse(Int,ARGS[1]), require_secret_for_access=false)
